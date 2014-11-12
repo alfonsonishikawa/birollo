b52f5004a3c0508c05ea380713c71691785a4aec
alfonso.nishikawa@gmail.com <alfonso.nishikawa@gmail.com>
2014-11-12 11:53:54 +0100

    Añadido el sol y la tierra. Solucionado el problema de escalado de drawImage. El tag <canvas> ha de tener el height y width.

--- WebContent/virollo.js
+++ WebContent/virollo.js
@@ -2,70 +2,58 @@
 function virollizar( canvasContextMaster, canvasContextSlave ) {
 	
 	canvasContextMaster.profundidad$ = 5 ;
+	/** Valor que escala el efecto de profundidad. Cuanto mayor es el valor, menos se percibe. */
+	canvasContextMaster.escaladoProfundidad$ = 2 ;
+	/** La profundidad efectiva es la profundidad dividida entre el escalado */
+	canvasContextMaster.profundidadEfectiva$ = 5/2 ;
+	
 	canvasContextMaster.ancho$ = canvasContextMaster.canvas.width ;
 	canvasContextMaster.alto$ = canvasContextMaster.canvas.height ;
 	canvasContextMaster.centroX$ = this.ancho$ / 2 ;
 	canvasContextMaster.centroY$ = this.alto$ / 2 ;
 	canvasContextMaster.slave$ = canvasContextSlave ;
-	
+
 	
 	canvasContextMaster.realFillRect$ = canvasContextMaster.fillRect ;
-	
-	/**
-	 * Dada unas coordenadas, indica el desplazamiento X e Y
-	 * 
-	 * @Entrada:
-	 *   1)		/1 de tipo {x:, y:}
-	 * 	 2) 	/1 de tipo number (sólo coordenada X)
-	 *   3)     /2 de tipo (number, number) (X,Y)
-	 *   4)     /2 de tipo (undefined, number) (_,Y)
-	 */
-	canvasContextMaster.incAlCentro = function(packetOrXOrUndefined, yOrUndefined) {
-		var incX = undefined,
-			incY = undefined ;
-		
-		// 1)
-		if (typeof(packetOrXOrUndefined) == 'object') {
-			incX = packetOrXOrUndefined.x - this.centroX$ ;
-			incY = packetOrXOrUndefined.y - this.centroY$ ;
-			return {x:incX, y:incY} ;
-		}
-		
-		// 2) y 3)
-		if (typeof(packetOrXOrUndefined) == "number") {
-			incX = packetOrXOrUndefined - this.centroX$ ;
-			if (typeof(yOrUndefined) != "undefined" && typeof(yOrUndefined) != "number") {
-				throw "Segundo parámetro inválido. Se esperaba number o undefined" ;
-			}
-			if (typeof(yOrUndefined) == "number") { // 3)
-				incY = yOrUndefined - this.centroY$ ;
-			}
-			return {x:incX, y:incY} ;
-		}
-		
-		//4)
-		if (typeof(yOrUndefined) == "number") {
-			incY = yOrUndefined - this.centroY$ ;
-			return {x:incX, y:incY} ;
-		}
-		
-		throw "Parámetros inválidos" ;
-	} ;
+	canvasContextMaster.realDrawImage$ = canvasContextMaster.drawImage ;
 	
 	canvasContextMaster.setProfundidad = function ( profundidad ) {
 		this.profundidad$ = profundidad ;
+		this.profundidadEfectiva$ = profundidad / this.escaladoProfundidad$ ;
 	} ;
 	
 	canvasContextMaster.getProfundidad = function () {
 		return this.profundidad$ ;
 	} ;
+
+	canvasContextMaster.setEscaladoProfundidad = function (escalado) {
+		this.escaladoProfundidad$ = escalado ;
+		this.profundidadEfectiva$ = this.profundidad$ / this.escaladoProfundidad$ ;
+	} ;
+	
+	canvasContextMaster.getEscaladoProfundidad = function (escalado) {
+		return this.escaladoProfundidad$ ;
+	} ;
 	
 	canvasContextMaster.fillRect = function(x, y, w, h) {
-		var incCoords = this.incAlCentro(x, y) ;
-		this.realFillRect$.apply(this, [x - this.profundidad$, y, w, h]) ;
+		this.realFillRect$.apply(this, [x - this.profundidadEfectiva$, y, w, h]) ;
 		
 		this.slave$.fillStyle = this.fillStyle ;
-		this.slave$.fillRect(x + this.profundidad$, y, w, h) ;
+		this.slave$.fillRect(x + this.profundidadEfectiva$, y, w, h) ;
+	} ;
+	
+	canvasContextMaster.drawImage = function(image, dxOrSx, dyOrSy, dwOrSw, dhOrSh, dx, dy, dw, dh) {
+		if (typeof(dwOrSw) == "undefined") {
+			this.realDrawImage$.apply(this, [image, dxOrSx - this.profundidadEfectiva$, dyOrSy]) ;
+			this.slave$.drawImage(image, dxOrSx + this.profundidadEfectiva$, dyOrSy) ;
+		} else if (typeof(dx) == "undefined") {
+			this.realDrawImage$.apply(this, [image, dxOrSx - this.profundidadEfectiva$, dyOrSy, dwOrSw, dhOrSh]) ;
+			this.slave$.drawImage(image, dxOrSx + this.profundidadEfectiva$, dyOrSy, dwOrSw, dhOrSh) ;
+		} else {
+			this.realDrawImage$.apply(this, [image, dxOrSx, dyOrSy, dwOrSw, dhOrSh, dx - this.profundidadEfectiva$, dy, dw, dh]) ;
+			this.slave$.drawImage(image, dxOrSx, dyOrSy, dwOrSw, dhOrSh, dx + this.profundidadEfectiva$, dy, dw, dh) ;
+		}
+		
 	} ;
 	
 	canvasContextMaster.beginPath = function() {
